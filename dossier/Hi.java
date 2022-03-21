b623cb893ac722893b553aca0ee9aa7cada944f0
mohabaw <89495030+mohabaw@users.noreply.github.com>
2022-03-21 11:00:21 +0100

    Update Hi.java

--- dossier/Hi.java
+++ dossier/Hi.java
@@ -1,9 +1,9 @@
-public class Hello {
+public class Hi {
 
 	public static void main(String[] args) {
 		
 		for (int i=1; i<=10;i++) {
-			System.out.println("Hello world ! " +i);
+			System.out.println("Hi man ! " +i);
 		}
 		
 		
